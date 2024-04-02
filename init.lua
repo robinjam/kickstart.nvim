ee76be32616c0432262246929dc60e0bcde9ffd7
James Robinson <james@robinjam.net>
2024-04-02 20:55:18 +0100

    Add clangd, tsserver, solargraph

--- init.lua
+++ init.lua
@@ -606,7 +606,7 @@ require('lazy').setup({
       --  - settings (table): Override the default settings passed when initializing the server.
       --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
       local servers = {
-        -- clangd = {},
+        clangd = {},
         -- gopls = {},
         pyright = {},
         rust_analyzer = {},
@@ -616,8 +616,8 @@ require('lazy').setup({
         --    https://github.com/pmizio/typescript-tools.nvim
         --
         -- But for many setups, the LSP (`tsserver`) will work just fine
-        -- tsserver = {},
-        --
+        tsserver = {},
+        solargraph = {},
 
         lua_ls = {
           -- cmd = {...},
