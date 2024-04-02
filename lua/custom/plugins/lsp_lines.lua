318fade3f1bd8f998fb55aa5816b9899fa36359f
James Robinson <james@robinjam.net>
2024-04-02 20:11:14 +0100

    Add lsp_lines

---
+++ lua/custom/plugins/lsp_lines.lua
@@ -0,0 +1,12 @@
+return {
+  'https://git.sr.ht/~whynothugo/lsp_lines.nvim',
+  config = function()
+    require('lsp_lines').setup()
+    vim.keymap.set('', '<Leader>l', require('lsp_lines').toggle, { desc = 'Toggle lsp_lines' })
+
+    -- Disable virtual_text since it's redundant due to lsp_lines.
+    vim.diagnostic.config {
+      virtual_text = false,
+    }
+  end,
+}
