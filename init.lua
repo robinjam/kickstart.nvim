4c488d05829927a44d7e65c285377e9fedad892f
James Robinson <james@robinjam.net>
2024-04-02 20:10:14 +0100

    Add LSP function signature help

--- init.lua
+++ init.lua
@@ -670,6 +670,7 @@ require('lazy').setup({
       --  into multiple repos for maintenance purposes.
       'hrsh7th/cmp-nvim-lsp',
       'hrsh7th/cmp-path',
+      'hrsh7th/cmp-nvim-lsp-signature-help',
     },
     config = function()
       -- See `:help cmp`
@@ -735,6 +736,7 @@ require('lazy').setup({
           { name = 'nvim_lsp' },
           { name = 'luasnip' },
           { name = 'path' },
+          { name = 'nvim_lsp_signature_help' },
         },
       }
     end,
