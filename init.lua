77f82a0bba13b9a79edfe7b684046a8b4273066c
James Robinson <james.robinson@digital.cabinet-office.gov.uk>
2024-04-03 10:38:01 +0100

    Include hidden files in Telescope file picker

--- init.lua
+++ init.lua
@@ -389,7 +389,12 @@ require('lazy').setup({
         --     i = { ['<c-enter>'] = 'to_fuzzy_refine' },
         --   },
         -- },
-        -- pickers = {}
+        pickers = {
+          find_files = {
+            hidden = true,
+            file_ignore_patterns = { '.git/' },
+          },
+        },
         extensions = {
           ['ui-select'] = {
             require('telescope.themes').get_dropdown(),
