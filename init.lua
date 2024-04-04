9bcef3ea59242b834ab0afc9fc2e1dbf545bc983
James Robinson <james.robinson@digital.cabinet-office.gov.uk>
2024-04-04 12:05:34 +0100

    Add terraformls

--- init.lua
+++ init.lua
@@ -621,6 +621,7 @@ require('lazy').setup({
         --    https://github.com/pmizio/typescript-tools.nvim
         --
         -- But for many setups, the LSP (`tsserver`) will work just fine
+        terraformls = {},
         tsserver = {},
         solargraph = {},
 
