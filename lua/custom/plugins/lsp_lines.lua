33ae7d5239bd806e0b51789d468c59c74c87c7dc
James Robinson <james.robinson@digital.cabinet-office.gov.uk>
2024-04-11 09:21:04 +0100

    lsp_lines: toggle virtual_lines AND virtual_text
    (Also update which-key hint)

--- lua/custom/plugins/lsp_lines.lua
+++ lua/custom/plugins/lsp_lines.lua
@@ -2,11 +2,13 @@ return {
   'https://git.sr.ht/~whynothugo/lsp_lines.nvim',
   config = function()
     require('lsp_lines').setup()
-    vim.keymap.set('', '<Leader>l', require('lsp_lines').toggle, { desc = 'Toggle lsp_lines' })
 
-    -- Disable virtual_text since it's redundant due to lsp_lines.
-    vim.diagnostic.config {
-      virtual_text = false,
-    }
+    local toggle = function()
+      local new_value = not vim.diagnostic.config().virtual_lines
+      vim.diagnostic.config { virtual_lines = new_value, virtual_text = not new_value }
+    end
+
+    toggle()
+    vim.keymap.set('', '<Leader>l', toggle, { desc = 'Toggle [l]sp_lines' })
   end,
 }
